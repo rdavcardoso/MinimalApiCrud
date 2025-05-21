de15d61d2da5ed058cce27c22396dbd60a6db682
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-05-21 20:27:42 -0300

    Entidade produto ajustada com Data Annotations.

--- MinimalApiCrud/Models/Produto.cs
+++ MinimalApiCrud/Models/Produto.cs
@@ -1,8 +1,14 @@
-﻿namespace MinimalApiCrud.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalApiCrud.Models;
 
 public class Produto
 {
     public int Id { get; set; }
+    
+    [Required]
     public string Nome { get; set; } = string.Empty;
+    
+    [Range(0.01, double.MaxValue)]
     public decimal Preco { get; set; }
 }
